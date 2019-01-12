b48a36c420685ddd24e2f5d0c86d1e5b1cc9cc23
Uk <ukchukundah@gmail.com>
2019-01-12 01:23:03 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "import/no-extraneous-dependencies": "off",
+    "object-shorthand": "off",
+    "object-curly-newline": "off",
+    "camelcase": "off",
+    "yoda": "off",
+    "comma-dangle": ["error", "never"],
+    "max-len": ["error", 120, 2, { "ignoreComments": true }],
+    "no-unused-vars": ["warn", { "vars": "local", "args": "none" }],
+    "no-cond-assign": ["error", "except-parens"],
+    "no-param-reassign": "off",
+    "no-nested-ternary": "off",
+    "no-trailing-spaces": "off",
+    "import/prefer-default-export": "off",
+    "linebreak-style": "off",
+    "no-continue": "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
+  },
+  "globals": {
+    "use": true
+  }
+};
